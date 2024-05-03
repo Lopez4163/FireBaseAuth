1aa6848d8235c33b6f60aa634ec880605a6db8af
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-05-02 22:46:08 -0400

    Update index.js

--- FireAuth/src/firebase/index.js
+++ FireAuth/src/firebase/index.js
@@ -12,14 +12,7 @@ import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // firebaseConfig.js
 const firebaseConfig = {
-  apiKey: "AIzaSyBAVXLKD7OJXGxA3B1pnujxuDnnfDH8mfo",
-  authDomain: "authtest-f7199.firebaseapp.com",
-  projectId: "authtest-f7199",
-  storageBucket: "authtest-f7199.appspot.com",
-  messagingSenderId: "596678976219",
-  appId: "1:596678976219:web:edc2046162b4d928b762fe",
-  measurementId: "G-C6EBDSBYF0",
-}
+
 //providers
 const provider = new GoogleAuthProvider()
 
